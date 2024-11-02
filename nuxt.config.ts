290db7f9fe8b9a08d9d2227f84ebddc5cb7a707b
Vasyl Tsiutsyk <v.tsyutsyk@gmail.com>
2024-11-02 12:55:08 +0200

    added meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Vasyl Tsiutsyk | Frontend Developer',
+      meta: [
+        { name: 'description', content: 'A brief description of your portfolio site' },
+        { name: 'keywords', content: 'portfolio, web developer, frontend developer, etc.' },
+        { property: 'og:title', content: 'Vasyl Tsiutsyk | Frontend Developer' },
+        { property: 'og:description', content: 'A brief description of your portfolio site' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: '/path/to/your-image.jpg' },
+        { property: 'og:url', content: 'https://vasyl-tsiutsyk.pages.dev/' },
+      ],
+      link: [{ rel: 'canonical', href: 'https://vasyl-tsiutsyk.pages.dev/' }],
+    },
+  },
+});

342c938d4e5f3a22cf26c8f014831adbc2865f5d
Vasyl Tsiutsyk <v.tsyutsyk@gmail.com>
2024-11-02 13:01:37 +0200

    added meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,20 @@ export default defineNuxtConfig({
     head: {
       title: 'Vasyl Tsiutsyk | Frontend Developer',
       meta: [
-        { name: 'description', content: 'A brief description of your portfolio site' },
+        {
+          name: 'description',
+          content:
+            'Experienced Frontend Developer with over 4 years of expertise in JavaScript, CSS, SCSS, SASS, Tailwind CSS, Bootstrap, and Unocss. Proficient in Vue.js (Vue2/Vue3) and Nuxt, I create stunning, interactive animations and user-friendly interfaces for your web projects. Available for remote work only.',
+        },
         { name: 'keywords', content: 'portfolio, web developer, frontend developer, etc.' },
         { property: 'og:title', content: 'Vasyl Tsiutsyk | Frontend Developer' },
-        { property: 'og:description', content: 'A brief description of your portfolio site' },
+        {
+          property: 'og:description',
+          content:
+            'Experienced Frontend Developer with over 4 years of expertise in JavaScript, CSS, SCSS, SASS, Tailwind CSS, Bootstrap, and Unocss. Proficient in Vue.js (Vue2/Vue3) and Nuxt, I create stunning, interactive animations and user-friendly interfaces for your web projects. Available for remote work only.',
+        },
         { property: 'og:type', content: 'website' },
-        { property: 'og:image', content: '/path/to/your-image.jpg' },
+        { property: 'og:image', content: 'public/me.webp' },
         { property: 'og:url', content: 'https://vasyl-tsiutsyk.pages.dev/' },
       ],
       link: [{ rel: 'canonical', href: 'https://vasyl-tsiutsyk.pages.dev/' }],

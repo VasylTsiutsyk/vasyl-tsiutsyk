e7db574a5d6d2d9f51bbf16c09762d65874e4255
Vasyl Tsiutsyk <v.tsyutsyk@gmail.com>
2024-11-07 14:24:05 +0200

    ui fixes

--- app.vue
+++ app.vue
@@ -5,27 +5,5 @@
 </template>
 
 <script setup>
-// import { useMobileDetection } from 'vue3-mobile-detection';
-
 const layout = 'default';
-
-// const { isMobile } = useMobileDetection();
-// const isMobileRef = computed(() => isMobile());
-
-// const updateBodyClass = () => {
-//   const body = document.body;
-//   if (!body) return;
-
-//   if (isMobileRef.value) {
-//     body.classList.remove('_pc');
-//     body.classList.add('_mobile');
-//   } else {
-//     body.classList.remove('_mobile');
-//     body.classList.add('_pc');
-//   }
-// };
-
-// onMounted(() => {
-//   updateBodyClass();
-// });
 </script>
